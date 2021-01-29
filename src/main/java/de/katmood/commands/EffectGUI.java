44b5001d98285eb92639435041a0639db43d7211
noname28439 <dohle.leon@web.de>
2021-01-29 17:20:42 +0100

    Added EffectGUI Command

---
+++ src/main/java/de/katmood/commands/EffectGUI.java
@@ -0,0 +1,21 @@
+package de.katmood.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class EffectGUI implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command arg1, String arg2, String[] args) {
+		
+		
+		
+		return false;
+	}
+
+	
+	
+	
+	
+}
