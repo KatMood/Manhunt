1aeb96f1c44115edc7e3c043dc776f3fa5fa639b
noname28439 <dohle.leon@web.de>
2021-02-01 20:31:21 +0100

    Added Inventory Load method

---
+++ src/main/java/de/katmood/commands/InventoryManager.java
@@ -0,0 +1,31 @@
+package de.katmood.commands;
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import de.katmood.manhunt.Manhunt;
+
+public class InventoryManager {
+	
+	public static void saveInv(String path, Inventory toSave) {
+		
+	}
+	
+	public static Inventory loadInv(String configurationPath) {
+		Inventory teamInv = Bukkit.createInventory(null, 3*9);
+		if(Manhunt.plugin.getConfig().isSet(configurationPath)) {
+			ItemStack[] contents = teamInv.getContents();
+			List<?> toLoadInv = Manhunt.plugin.getConfig().getList(configurationPath);
+			for(int i = 0; i<toLoadInv.size();i++) {
+				System.out.println("Loading... "+toLoadInv.get(i));
+				contents[i] = (ItemStack) toLoadInv.get(i);
+			}	
+			teamInv.setContents(contents);
+		}
+		return teamInv;
+	}
+	
+}
