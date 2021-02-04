67034325e2274216654cc8aac1629516903d0984
noname28439 <dohle.leon@web.de>
2021-02-04 22:25:10 +0100

    Added Default World, the player gets teleportet to on each join and save/load in/from config, also a method to set this worlds name

--- src/main/java/de/katmood/commands/WorldManager.java
+++ src/main/java/de/katmood/commands/WorldManager.java
@@ -1,6 +1,7 @@
 package de.katmood.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.WorldCreator;
 import org.bukkit.WorldType;
@@ -8,23 +9,33 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import de.katmood.manhunt.Manhunt;
+
 public class WorldManager implements Listener{
 	
+	
+	public static String mainWorldNamePath = "MAIN_WORLD";
+	public static String mainWorldName = "world";
+	
 	public static void saveMainWorld() {
-		
+		Manhunt.plugin.getConfig().set(mainWorldNamePath, mainWorldName);
+		Manhunt.plugin.saveConfig();
 	}
 	
 	public static void loadMainWorld() {
-		
+		if(Manhunt.plugin.getConfig().isSet(mainWorldNamePath))
+			mainWorldName = Manhunt.plugin.getConfig().getString(mainWorldNamePath);
 	}
 	
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent e) {
-		
+		e.getPlayer().teleport(new Location(Bukkit.getWorld(mainWorldName), e.getPlayer().getLocation().getX(), e.getPlayer().getLocation().getY(), e.getPlayer().getLocation().getZ()));
 	}
 
-	public static void createBackup(World toBackup) {
-	
+	public static void createBackup(String BackupWorldName, World toBackup) {
+		WorldCreator wc = new WorldCreator(BackupWorldName);
+		wc.copy(toBackup);
+		wc.createWorld();
 	}
 	
 	public static void deleteWorld(String worldName) {
@@ -32,7 +43,6 @@ public class WorldManager implements Listener{
 	}
 	
 	public static World createNewWorld(String worldName) {
-		
 		WorldCreator wc = new WorldCreator(worldName);
 		wc.type(WorldType.NORMAL);
 		wc.generateStructures(true);
@@ -42,7 +52,7 @@ public class WorldManager implements Listener{
 	}
 	
 	public static void setWorldAsMainWorld(String worldName) {
-		
+		mainWorldName = worldName;
 	}
 	
 	
